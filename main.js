dfdff90f392084e56774a264c39b93b00a71910c
unknown <USRSIS0129@ESTSIS0129.cobeca.com.ve>
2016-05-20 15:35:49 -0400

    initial comit

--- main.js
+++ main.js
@@ -1,36 +1,38 @@
 const electron = require('electron')
-// Module to control application life.
 const app = electron.app
-// Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
+let miniWindow
 
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
-
-  // and load the index.html of the app.
+function createWindow() {
+  mainWindow = new BrowserWindow({ width: 250, height: 250, frame: false })
+  mainWindow.setMenu(null)
   mainWindow.loadURL(`file://${__dirname}/index.html`)
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
   mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     mainWindow = null
+  })
+}
+
+function createMiniWindow() {
+  miniWindow = new BrowserWindow({ width: 250, height: 75, frame: false })
+  miniWindow.setMenu(null)
+  miniWindow.loadURL(`file://${__dirname}/index-mini.html`)
+
+  miniWindow.on('closed', function () {
+    miniWindow = null
   })
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', function () {
+  createWindow()
+})
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
@@ -51,3 +53,17 @@ app.on('activate', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+const ipcMain = electron.ipcMain
+ipcMain.on('close-player', function (event, args) {
+  console.log(event)
+  console.log(args)
+  //app.quit()
+})
+ipcMain.on('show-mini-player', function () {
+  createMiniWindow()
+  mainWindow.hide()
+})
+ipcMain.on('show-player', function () {
+  mainWindow.show()
+  miniWindow.close()
+})
