ddaa44b75dff2f0339d94a9cd5510765769b493f
unknown <USRSIS0129@ESTSIS0129.cobeca.com.ve>
2016-06-01 16:56:42 -0400

    Added settings controller

--- client/app/player/player.js
+++ client/app/player/player.js
@@ -5,27 +5,13 @@ app.controller('PlayerController', ['$scope', function ($scope) {
   $scope.showMiniPlayer = function(){
     ipcRenderer.send('show-mini-player')
   }
-  
+
   $scope.showSettings = function(){
     ipcRenderer.send('show-settings')
   }
-  
+
   $scope.closePlayer = function(){
     ipcRenderer.send('close-player')
   }
-  // let miniPlayerButton = document.getElementById('show-mini-player')
-  // miniPlayerButton.addEventListener('click', function () {
-  //   ipcRenderer.send('show-mini-player')
-  // })
-
-  // let closeButton = document.getElementById('close-player')
-  // closeButton.addEventListener('click', function () {
-  //   ipcRenderer.send('close-player')
-  // })
-
-  // let settingsButton = document.getElementById('show-settings')
-  // settingsButton.addEventListener('click', function () {
-  //   ipcRenderer.send('show-settings')
-  // })
 
 }])
--- client/app/settings/settings.js
+++ client/app/settings/settings.js
@@ -1,119 +1,104 @@
 'strict mode'
 const ipcRenderer = require('electron').ipcRenderer
-// If using in renderer process
 const {dialog} = require('electron').remote
 const fs = require('fs')
 
-const extensions = ['MP3', 'FLAC']
+app.controller('SettingsController', ['$scope', function ($scope) {
+  $scope.extensions = ['MP3', 'FLAC']
+  $scope.totalFiles = 0
+  $scope.songs = []
 
-let totalFiles = 0
-let songs = []
-
-let menu = document.querySelector('.items')
-menu.addEventListener('click', function(e) {
-  showContent(e.target.id)
-})
-
-showContent = function(id) {
-  let items = document.querySelectorAll('.items li')
-  for (let i = 0; i < items.length; i++) {
-    document.querySelector('#' + items[i].id + '-content').style.display = 'none'
-    document.querySelector('#' + items[i].id).style.borderBottom = '5px solid transparent'
-    document.querySelector('#' + items[i].id).style.transition = 'all 0.5s ease'
+  $scope.showContent = function (e) {
+    let id = e.target.id
+    let items = document.querySelectorAll('.items li')
+    for (let i = 0; i < items.length; i++) {
+      document.querySelector('#' + items[i].id + '-content').style.display = 'none'
+      document.querySelector('#' + items[i].id).style.borderBottom = '5px solid transparent'
+      document.querySelector('#' + items[i].id).style.transition = 'all 0.5s ease'
+    }
+    document.querySelector('#' + id + '-content').style.display = 'block'
+    document.querySelector('#' + id).style.borderBottom = '5px solid cyan'
+    document.querySelector('#' + id).style.transition = 'all 0.5s ease'
   }
-  document.querySelector('#' + id + '-content').style.display = 'block'
-  document.querySelector('#' + id).style.borderBottom = '5px solid cyan'
-  document.querySelector('#' + id).style.transition = 'all 0.5s ease'
-}
 
-let closeSettings = document.getElementById('close-settings')
-closeSettings.addEventListener('click', function() {
-  ipcRenderer.send('close-settings')
-})
-
-let buttonAdd = document.getElementById('buttonAdd')
-buttonAdd.addEventListener('click', function() {
-    showFileDialog()
-})
-
-function showFileDialog(){
-  dialog.showOpenDialog({
-    properties: ['openFile', 'openDirectory', 'multiSelections']
-  }, function(result) {
-    addFolders(result)
-  })
-}
-
-function addFolders(folders) {
-  var folderList = document.querySelector('.folders-list')
-  for (let i = 0; i < folders.length; i++) {
-    let newItem = document.createElement('li')
-    newItem.innerHTML = folders[i]
-    newItem.id = 'folder-' + i
-    newItem.setAttribute('title', folders[i])
-    folderList.appendChild(newItem)
+  $scope.closeSettings = function () {
+    ipcRenderer.send('close-settings')
   }
-  updateSongs(folders)
-}
 
-function updateSongs(folders) {
-  for (let i = 0; i < folders.length; i++) {
-    processFolder(folders[i])
+  $scope.showFileDialog = function () {
+    dialog.showOpenDialog({
+      properties: ['openFile', 'openDirectory', 'multiSelections']
+    }, function (result) {
+      addFolders(result)
+    })
   }
-}
 
-function processFolder(folder) {
-  var folderSeparator = ''
-  if (folder.indexOf('/') >= 0) {
-    folderSeparator = '/'
-  } else {
-    folderSeparator = '\\'
+  $scope.toggleSelected = function (element) {
+    var currentItem = document.getElementById(element)
+    if (currentItem.className == 'selected') {
+      currentItem.className = ''
+    } else {
+      currentItem.className = 'selected'
+    }
   }
-  var files = fs.readdir(folder, function(err, files) {
-    for (var index in files) {
-      try {
-        var currentFile = folder + folderSeparator + files[index]
-        if (fs.lstatSync(currentFile).isDirectory()) {
-          processFolder(currentFile)
-        } else {
-          if (extensions.indexOf(getExtension(currentFile).toUpperCase()) >= 0) {
-            songs.push(currentFile)
-          }
-        }
-      } catch (err) {
-        ipcRenderer.send('log', err.message)
+
+  $scope.removeFolder = function () {
+    var items = document.querySelectorAll('.folders-list li.selected')
+    for (let i = items.length - 1; i >= 0; i--) {
+      if (items[i].className == 'selected') {
+        document.querySelector('.folders-list').removeChild(items[i])
       }
     }
-    document.getElementById('total-songs').innerHTML = 'Total songs (' + songs.length + ')'
-  })
-  document.getElementById('total-songs').innerHTML = 'Total songs (' + songs.length + ')'
-}
-
-function getExtension(file) {
-  return file.substr(file.lastIndexOf('.') + 1).trim()
-}
-
-document.querySelector('.folders-list').addEventListener('click', function(e) {
-  if (e.target && e.target.nodeName === 'LI') {
-    toggleSelected(e.target.id)
   }
-})
 
-function toggleSelected(element) {
-  var currentItem = document.getElementById(element)
-  if (currentItem.className == 'selected') {
-    currentItem.className = ''
-  } else {
-    currentItem.className = 'selected'
+  function addFolders(folders) {
+    var folderList = document.querySelector('.folders-list')
+    for (let i = 0; i < folders.length; i++) {
+      let newItem = document.createElement('li')
+      newItem.innerHTML = folders[i]
+      newItem.id = 'folder-' + i
+      newItem.setAttribute('title', folders[i])
+      folderList.appendChild(newItem)
+    }
+    updateSongs(folders)
   }
-}
 
-let removeButton = document.getElementById('buttonRemove')
-removeButton.addEventListener('click', function() {
-  var items = document.querySelectorAll('.folders-list li.selected')
-  for (let i = items.length - 1; i >= 0; i--) {
-    if (items[i].className == 'selected') {
-      document.querySelector('.folders-list').removeChild(items[i])
+  function updateSongs(folders) {
+    for (let i = 0; i < folders.length; i++) {
+      processFolder(folders[i])
     }
   }
-})
+
+  function processFolder(folder) {
+    var folderSeparator = ''
+    if (folder.indexOf('/') >= 0) {
+      folderSeparator = '/'
+    } else {
+      folderSeparator = '\\'
+    }
+    var files = fs.readdir(folder, function (err, files) {
+      for (var index in files) {
+        try {
+          var currentFile = folder + folderSeparator + files[index]
+          if (fs.lstatSync(currentFile).isDirectory()) {
+            processFolder(currentFile)
+          } else {
+            if (extensions.indexOf(getExtension(currentFile).toUpperCase()) >= 0) {
+              songs.push(currentFile)
+            }
+          }
+        } catch (err) {
+          ipcRenderer.send('log', err.message)
+        }
+      }
+      document.getElementById('total-songs').innerHTML = 'Total songs (' + songs.length + ')'
+    })
+    document.getElementById('total-songs').innerHTML = 'Total songs (' + songs.length + ')'
+  }
+
+  function getExtension(file) {
+    return file.substr(file.lastIndexOf('.') + 1).trim()
+  }
+
+}])
+
